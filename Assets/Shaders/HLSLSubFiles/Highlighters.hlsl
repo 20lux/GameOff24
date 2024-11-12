1eeabde91d91387e6a2b4fa581e76bba78a453ab
jeremythedrawer <jedwards184@outlook.com>
2024-11-12 19:56:13 +1100

    fixed vertices sticking to origin

--- Assets/Shaders/HLSLSubFiles/Highlighters.hlsl
+++ Assets/Shaders/HLSLSubFiles/Highlighters.hlsl
@@ -1,6 +1,6 @@
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
 
-float4 PulsingBloom (bool isOn, float4 textureInput, float frequency, float magnitude, float offset)
+float3 PulsingBloomVert (bool isOn, float3 In, float frequency, float magnitude, float offset)
 {
     if (isOn > 0.5)
     {
@@ -8,9 +8,22 @@ float4 PulsingBloom (bool isOn, float4 textureInput, float frequency, float magn
     
         t = sin(t * frequency) * 0.5 + offset;
     
-        textureInput = float4 (RGBToHSV(textureInput), textureInput.w);
-        textureInput = float4 (textureInput.x, textureInput.y, textureInput.z * t, textureInput.w);
-        textureInput = float4(HSVToRGB(textureInput), textureInput.w);
+        In *= t;
     }
-        return textureInput;  
+    return In;
+}
+
+float3 PulsingBloomFrag (bool isOn, float3 In, float frequency, float magnitude, float offset)
+{
+    if (isOn > 0.5)
+    {
+        float t = _Time.y;
+    
+        t = sin(t * frequency) * 0.5 + offset;
+    
+        In = RGBToHSV(In);
+        In.z *= t;
+        In = HSVToRGB(In);
+    }
+    return In;
 }
