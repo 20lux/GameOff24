df2347c828a924d2755f808a94ef783ac1c9b7c9
Anne Truong <anne.truongxx@gmail.com>
2024-11-15 15:30:58 +1100

    Added spotlight fade in animation and updated fragment scripts. Changed celestial light system to activate via first fragment collected

--- Assets/Scripts/CelestialLightSystem/CelestialController.cs
+++ Assets/Scripts/CelestialLightSystem/CelestialController.cs
@@ -7,33 +7,42 @@ public class CelestialController : MonoBehaviour
 
     private bool celestialLeftInput;
     private bool celestialRightInput;
+    private bool isActive = false;
     private int rotDir = 0;
 
     private void Update()
     {
-        // Cache inputs for celestial rotation.
-        celestialLeftInput = InputManager.GetCelestialLeft;
-        celestialRightInput = InputManager.GetCelestialRight;
-        
-        if (celestialLeftInput && celestialRightInput)
+        if(isActive)
         {
-            return;
-        } else if (celestialLeftInput)
-        {
-            rotDir = 1;
-        } else if (celestialRightInput)
-        {
-            rotDir = -1;
-        } else
-        {
-            return;
-        }
+            // Cache inputs for celestial rotation.
+            celestialLeftInput = InputManager.GetCelestialLeft;
+            celestialRightInput = InputManager.GetCelestialRight;
+            
+            if (celestialLeftInput && celestialRightInput)
+            {
+                return;
+            } else if (celestialLeftInput)
+            {
+                rotDir = 1;
+            } else if (celestialRightInput)
+            {
+                rotDir = -1;
+            } else
+            {
+                return;
+            }
 
-        // Perform rotation with given input.
-        float yOffset = celestialBody.transform.eulerAngles.y + (Time.deltaTime * rotationSpeed * rotDir);
-        celestialBody.transform.rotation = Quaternion.Euler(
-                                            celestialBody.transform.eulerAngles.x, 
-                                            yOffset, 
-                                            celestialBody.transform.eulerAngles.z);
+            // Perform rotation with given input.
+            float yOffset = celestialBody.transform.eulerAngles.y + (Time.deltaTime * rotationSpeed * rotDir);
+            celestialBody.transform.rotation = Quaternion.Euler(
+                                                celestialBody.transform.eulerAngles.x, 
+                                                yOffset, 
+                                                celestialBody.transform.eulerAngles.z);
+        }
+    }
+
+    public void ActivateSundialPower()
+    {
+        isActive = true;
     }
 }
