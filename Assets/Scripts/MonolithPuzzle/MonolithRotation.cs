5b621826d5ba264cfaaacb1408926ffa257b3f92
Anne Truong <anne.truongxx@gmail.com>
2024-11-24 15:21:07 +1100

    Adjusting fall level monolith puzzle

--- Assets/Scripts/MonolithPuzzle/MonolithRotation.cs
+++ Assets/Scripts/MonolithPuzzle/MonolithRotation.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class MonolithRotation : MonoBehaviour
 {
 
-    [Tooltip("Enter degrees for monolith to rotate each round - should be unique to each monolith and value of 45, 90 or 135")]
-    public float yDegrees = 45;
+    [Tooltip("Enter degrees for monolith to rotate each round - should be unique to each monolith and value of 30, 60 or 90")]
+    public float yDegrees = 30;
     private AudioSource audioSource;
     private const float XVAL = 0;
     private const float ZVAL = -90;
+    private float speed;
     public float yCorrect = 90f;
 
     private Quaternion initialRotation = Quaternion.identity;
@@ -18,12 +19,18 @@ public class MonolithRotation : MonoBehaviour
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        speed = audioSource.clip.length;
         currentRotation = initialRotation;
         nextRotation = currentRotation;
     }
 
     void Update()
     {
+        if (transform.rotation.y > 360)
+        {
+            transform.rotation = initialRotation;
+        }
+        
         if (isRotating)
         {
             transform.rotation = nextRotation;
@@ -49,10 +56,8 @@ public class MonolithRotation : MonoBehaviour
     {
         isRotating = true;
         audioSource.Play();
-        Debug.Log("Initiating rotation!");
 
         var yToTurn = nextRotation.eulerAngles.y + yDegrees;
         nextRotation = Quaternion.Euler(XVAL, yToTurn, ZVAL);
-        Debug.Log("Turned " + yDegrees + " degrees on Y axis!");
     }
 }
